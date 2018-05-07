2c5495b61548f281767228f9d3cbea036594e68b
MakerLab-ITAM <33268345+MakerLab-ITAM@users.noreply.github.com>
2018-05-07 13:12:32 -0500

    Clase cuadro creada
    Prueba de clase cuadro

---
+++ src/sudoku2/SudoCuadro.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku2;
+
+/**
+ *
+ * @author usuario
+ * Clase wrapper para cuadro unitario de sudoku 
+ */
+public class SudoCuadro {
+    
+}
